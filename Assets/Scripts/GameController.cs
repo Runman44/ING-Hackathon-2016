b0bf556b4b1e9a2bc5ace3506df79b64c9d4d426
Dennis Anderson <info@mranderson.nl>
2016-03-22 23:07:29 +0100

    Add Time text and new background image and landscape color

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System;
+
+
+public class GameController : MonoBehaviour 
+{
+	public GameController ()
+	{}
+
+	void Update () {		
+		TimeManager.Instance.time = TimeManager.Instance.time - 1;
+	}
+
+
+}
+
+
---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using System;
+
+public class TimeManager : Singleton<TimeManager> {
+	protected TimeManager () {} 
+
+	public int time = 100;
+}
+
+
+
+
