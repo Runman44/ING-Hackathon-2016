2fdce9fa1b9fa21681c07dde129fa4f600376695
Dennis Anderson <info@mranderson.nl>
2016-03-22 23:53:02 +0100

    Time & Count are fixed in this one.

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -4,18 +4,21 @@ using Endless2DTerrain;
 
 public class CoinPickup : MonoBehaviour {
 
-    void OnTriggerEnter(Collider item)
-    {
-        
-        //Assume only one terrain displayer at a time
-        var terrainDisplayer = GameObject.FindObjectOfType(typeof(TerrainDisplayer)) as TerrainDisplayer;        
-        if (terrainDisplayer != null && terrainDisplayer.PrefabManager != null && terrainDisplayer.PrefabManager.Pool != null)
-        {
-            terrainDisplayer.PrefabManager.Pool.Remove(this.gameObject);
+	void OnTriggerEnter(Collider item)
+	{
+		//Assume only one terrain displayer at a time
+		var terrainDisplayer = GameObject.FindObjectOfType(typeof(TerrainDisplayer)) as TerrainDisplayer;        
+		if (terrainDisplayer != null && terrainDisplayer.PrefabManager != null && terrainDisplayer.PrefabManager.Pool != null)
+		{
+			terrainDisplayer.PrefabManager.Pool.Remove(this.gameObject);
 
-			CoinManager.Instance.creditCount = CoinManager.Instance.creditCount + 1; 
-		
-        }
-    }
+			CoinManager.Instance.creditCount = CoinManager.Instance.creditCount + 1;
+			updateScore();
+		}
+	}
+
+	public void updateScore() {
+		GameObject.Find("Score Text").GetComponent<GUIText>().text = "Coins: " + CoinManager.Instance.creditCount;
+	}
 
 }
---
+++ Assets/Scripts/TimeController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System;
+
+
+public class TimeController : MonoBehaviour 
+{
+	void Start() {
+
+	}
+
+	void Update () {		
+
+		GameObject.Find("Time Text").GetComponent<GUIText>().text = "Time: " + TimeManager.Instance.time;
+
+		//print (DateTime.Now.ToString("ss"));
+	}
+
+
+}
+
+
