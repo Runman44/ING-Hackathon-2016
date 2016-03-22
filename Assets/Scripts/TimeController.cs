fbc8d063821b49f8e1c8ec76ca659bc17934df9d
Dennis Anderson <info@mranderson.nl>
2016-03-23 00:11:34 +0100

    Timer is now counting down.

--- Assets/Scripts/TimeController.cs
+++ Assets/Scripts/TimeController.cs
@@ -1,16 +1,23 @@
 ﻿using UnityEngine;
 using System;
+using UnityEngine.UI;
 
 
 public class TimeController : MonoBehaviour 
 {
-	void Start() {
 
+	public float myCoolTimer = 100;
+	public Text timerText;
+
+	void Start() {
+		timerText = GetComponent<Text> ();
 	}
 
 	void Update () {		
+		myCoolTimer -= Time.deltaTime;
+		print (myCoolTimer);
 
-		GameObject.Find("Time Text").GetComponent<GUIText>().text = "Time: " + TimeManager.Instance.time;
+		GameObject.Find("Time Text").GetComponent<GUIText>().text = "Time: " + Mathf.Round(myCoolTimer);
 
 		//print (DateTime.Now.ToString("ss"));
 	}
