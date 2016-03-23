202066df61dcbc83c5b096691cfd66971129c5cc
renebdeveloper <renebdeveloper@gmail.com>
2016-03-23 11:20:20 +0100

    elephant

---
+++ Assets/Scripts/AnimalCrashSound.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class AnimalCrashSound : MonoBehaviour {
+	public AudioClip impact;
+	AudioSource audio;
+
+	void Start() {
+		audio = GetComponent<AudioSource>();
+		audio.PlayOneShot(impact, 0.7F);
+	}
+
+	void OnCollisionEnter() {
+		audio.PlayOneShot(impact, 0.7F);
+	}
+}
--- Assets/Scripts/AnimalPickup.cs
+++ Assets/Scripts/AnimalPickup.cs
@@ -6,13 +6,12 @@ public class AnimalPickup : MonoBehaviour {
 
     void OnTriggerEnter(Collider item)
     {
-        
         //Assume only one terrain displayer at a time
         var terrainDisplayer = GameObject.FindObjectOfType(typeof(TerrainDisplayer)) as TerrainDisplayer;        
         if (terrainDisplayer != null && terrainDisplayer.PrefabManager != null && terrainDisplayer.PrefabManager.Pool != null)
         {
             terrainDisplayer.PrefabManager.Pool.Remove(this.gameObject);
-		
+
         }
     }
 
