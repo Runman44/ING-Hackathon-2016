f02a3e30726abb52c8131fdfefede8454a4952f9
Dennis Anderson <info@mranderson.nl>
2016-03-23 11:24:59 +0100

    GameOver

---
+++ Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameOverManager : MonoBehaviour {
+
+	protected GameOverManager () {}
+
+	Animator anim;
+	public GameObject rentepunten;
+	public GameObject coinspunten;
+
+	void Awake () {
+		anim = GetComponent<Animator> ();
+	}
+
+	public void onRestart(){
+		CoinManager.Instance.creditCount = 0;
+		RentepuntManager.Instance.creditCount = 0;
+		TimeManager.Instance.time = 100;
+
+		Application.LoadLevel (Application.loadedLevel);
+	}
+
+	public void onStop(){
+		Application.Quit();
+	}
+
+	void Update () {
+		if (Mathf.Round (TimeManager.Instance.time) <= 0f) {
+			anim.SetTrigger ("GameOver");
+
+
+			GameObject.Find ("Rentepunten").GetComponent<Text>().text = "Rentepunten: " + RentepuntManager.Instance.creditCount;
+			GameObject.Find ("CoinsPunten").GetComponent<Text>().text = "Coins: " + CoinManager.Instance.creditCount;
+		}
+	}
+	
+
+}
