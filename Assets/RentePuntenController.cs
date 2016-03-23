78123ca53d6cca3dc4e87585f88fa0ca639a14ea
Dennis Anderson <info@mranderson.nl>
2016-03-23 04:14:24 +0100

    Rentepunten in the game now. Counting them too

---
+++ Assets/RentePuntenController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RentePuntenController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject.Find ("RentePunten Text").GetComponent<GUIText> ().text = "Rentepunten: " + RentepuntManager.Instance.creditCount;
+	}
+}
--- Assets/Scripts/TimeController.cs
+++ Assets/Scripts/TimeController.cs
@@ -14,7 +14,6 @@ public class TimeController : MonoBehaviour
 	public Text timerText;
 
 	void Start() {
-//		StartCoroutine()
 	}
 
 	void Update () {
